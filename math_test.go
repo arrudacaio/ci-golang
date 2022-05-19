77e5ff753ac69c57ccf5e090ec692515e23f8dfd
Caio Arruda <caio.arruda@pagar.me>
2022-05-19 08:04:34 -0300

    feat: adds sum function and test

---
+++ math_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := Soma(15,15)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: %d. Esperado %d", total, 30)
+	}
+
+}
